b4f5c8d06ea7784458b779087e9f5bc1ddccac37
Gilbert Tuyishime <tuyigilbert97@gmail.com>
2018-11-14 08:51:26 -1000

    Endpoints for getting specific user

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
-
+const fs = require('fs');
+let users = require('./data/users.json');
+let parcels = require('./data/parcels.json');
+const filename = './data/parcels.json';
 
 app.use(express.static('public'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(require('./routes/index.routes'));
+
 // all pages
 app.get('/', function(req, res) {
 res.render("index.ejs");
@@ -47,9 +49,128 @@ app.get('/profile.ejs',function(req,res){
 });
 
 
+/* APIs */
 
 
-var port    =   process.env.PORT || 5000;
+/* All parcel */
+app.get('/api/v1/parcels/', function (req, res) {
+    if (parcels.length === 0) {
+       return res.json({ message: 'no parcels available', status: 202 });
+    } else {
+        return res.json(parcels);
+    }
+});
+
+/*Fetch a specific parcel delivery order*/
+app.get('/api/v1/parcels/:id', function (req, res) {
+    const p_id = req.params.id;
+    if (parcels.length === 0) {
+        return res.json({ message: 'no parcels available', status: 202 });
+    } else {
+        const specificparcel = parcels.find(function (sp) {
+            return sp.parcel_id == p_id;
+        });
+        if (!specificparcel) {
+            return res.json({ message: 'Parcel not found', status: 404 });
+        } else {
+           return  res.json(specificparcel);
+        }
+    }
+});
+
+/*Cancel the specific parcel delivery order*/
+
+app.put('/api/v1/parcels/:parcelid/cancel',  function (req, res) {
+    const id = req.params.parcelid;
+    if (parcels.length === 0) {
+        res.json({ message: 'no parcels available', status: 202 });
+    } else {
+
+        parcels = parcels.filter(function (p) {
+            return p.parcel_id != id;
+        });
+        fs.writeFileSync(filename, JSON.stringify(parcels), 'utf8', function (err) {
+            if (err) {
+                return res.json({ message: err });
+            }
+        });
+
+        return res.json({ message: 'parcels delivery odrerd canceled', status: 202 });
+    }
+});
+
+/*Create a parcel delivery order*/
+
+app.post('/api/v1/parcels/', function (req, res) {
+
+    const data = req.body;
+    let parcelid = { parcel_id: 1 };
+    if (parcels.length > 0) {
+        parcelid = { parcel_id: parcels.length + 1 };
+    }
+
+    let new_parcel = { parcelid, data };
+    parcels.push(new_parcel);
+    fs.writeFileSync(filename, JSON.stringify(parcels), 'utf8', function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+
+    return res.json({ message: 'Parcel delivery order created' });
+});
+
+
+
+/* all user */
+
+
+
+app.get('/api/v1/users/', function (req, res) {
+    if (users.length === 0) {
+       return res.json({ message: 'no users available', status: 202 });
+    } else {
+       return res.json(users);
+    }
+});
+
+/* a user by id */
+app.get('/api/v1/users/:id', function (req, res) {
+    const id = req.params.id;
+    const user = users.find(function (r) {
+        return r.user_id == id;
+    });
+    if (!user) {
+       return res.json({ message: 'User not found', status: 404 });
+    } else {
+       return res.json(user);
+    }
+});
+
+/*Fetch all parcel delivery orders by a specific user*/
+
+app.get('/api/v1/users/:userid/parcels',function (req, res) {
+    const id = req.params.userid;
+    if (parcels.length === 0) {
+        return res.json({ message: 'no parcels available', status: 202 });
+    } else {
+        const userparcels = parcels.filter(function (u) {
+            return u.user_id == id;
+        });
+        if (!userparcels) {
+           return res.json({ message: 'no parcel for this user', status: 404 });
+        } else {
+           return res.json(userparcels);
+        }
+    }
+});
+
+
+
+
+
+
+var port    =   process.env.PORT;
 app.listen(port, function() {
 })
 
--- routes/index.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.use('/api/v1/users', require('./user.routes'))
-router.use('/api/v1/parcels', require('./parcel.routes'))
-
-module.exports = router
